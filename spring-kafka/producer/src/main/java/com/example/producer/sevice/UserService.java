0111fcfa7418f5aae4e5bf78b3816169c96b35f0
ilya03071993 <celiwan@mail.ru>
2024-09-16 11:40:12 +0300

    Fix bags

--- spring-kafka/producer/src/main/java/com/example/producer/sevice/UserService.java
+++ spring-kafka/producer/src/main/java/com/example/producer/sevice/UserService.java
@@ -4,7 +4,6 @@ import com.example.producer.dto.UserDto;
 import com.example.producer.entity.User;
 import com.example.producer.mapper.UserMapper;
 import com.example.producer.repository.UserRepository;
-import com.example.producer.util.JsonUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -18,17 +17,12 @@ public class UserService {
     private final UserMapper userMapper;
     @Value("${message.retention-period}")
     private int retentionPeriod;
-    private final JsonUtils jsonUtils;
 
     public UserDto saveUser(String message) {
         User user = new User(null, message, LocalDateTime.now(), LocalDateTime.now().plusMinutes(retentionPeriod));
 
         User savedUser = userRepository.save(user);
 
-        UserDto userMapperDto = userMapper.toDto(savedUser);
-
-        jsonUtils.toJson(userMapperDto);
-
-        return userMapperDto;
+        return userMapper.toDto(savedUser);
     }
 }
